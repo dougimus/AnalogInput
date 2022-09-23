a296f77c800cb4a1a5b3d64deb956463af195fc1
Douglas Ward <30300064+dougimus@users.noreply.github.com>
2022-09-23 10:35:23 -0400

    voltage extension code

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,13 @@ void loop() {
   int iVal; 
  
   // read digitized value from the D1 Mini's A/D convertor 
-  iVal = analogRead(A0); 
+  iVal = analogRead(A0);
+
+  //do conversion to voltage float
+  float voltage = iVal * (3.3 / 1023.0);
  
   // print value to the USB port 
-  Serial.println("Digitized Value = " + String(iVal)); 
+  Serial.println("Digitized Value of " + String(iVal) + " is equivalent to an Analog Voltage = " + String(voltage) + "V"); 
  
   // wait 0.5 seconds (500 ms) 
   delay(500); 
