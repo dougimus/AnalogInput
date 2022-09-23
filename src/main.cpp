d602ba6ae55f51d1e40f1c335f24efd7aa79c54b
Douglas Ward <30300064+dougimus@users.noreply.github.com>
2022-09-23 10:56:05 -0400

    changed to simulate temp measurement and added statement of authorship

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,36 @@ void loop() {
   // read digitized value from the D1 Mini's A/D convertor 
   iVal = analogRead(A0);
 
-  //do conversion to voltage float
-  float voltage = iVal * (3.3 / 1023.0);
- 
+  //do conversion to temperature float
+  float temperature = iVal * (50 / 1023.0);
+  String judgement = "";
+  if(temperature < 10) {
+    judgement = "Cold!";
+  }
+  else if (temperature >= 10 && temperature < 15)
+  {
+    judgement = "Cool";
+  }
+  else if (temperature >= 15 && temperature < 25)
+  {
+    judgement = "Perfect";
+  }
+  else if (temperature >= 25 && temperature < 30)
+  {
+    judgement = "Warm";
+  }
+  else if (temperature >= 30 && temperature < 35)
+  {
+    judgement = "Hot";
+  }
+  else if (temperature > 35)
+  {
+    judgement = "Too Hot!";
+  }
+  
   // print value to the USB port 
-  Serial.println("Digitized Value of " + String(iVal) + " is equivalent to an Analog Voltage = " + String(voltage) + "V"); 
+  Serial.println("Digitized Value of " + String(iVal) + " is equivalent to a temperature input of " + String(temperature) + " Deg. C, which is " + judgement); 
  
-  // wait 0.5 seconds (500 ms) 
-  delay(500); 
+  // wait 2 seconds (2000 ms) 
+  delay(2000); 
 }
